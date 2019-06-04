459999d28a12007ff9a3069ebf845cbf65110eb2
Nelson <neskone@outlook.com>
2019-06-03 22:04:02 -0700

    more organized and works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,169 @@
+//VARIABLES
+const words = ['pug', 'shitzu', 'bulldog']
+
+//Empty variables to store values later
+let randomWord = "";
+let lettersOfWord = []
+let blanks = 0;
+let blanksAndCorrect = [];
+let wrongGuess = [];
+
+//Counter Variables
+let wins = 0;
+let losses = 0;
+let guessesRemaining = 9;
 
 
 
+// ALL FUNCTIONS
+/////////////////////////
 
 
+//__________________________________________________________
+//GAME START FUNCTION
+//__________________________________________________________
+function Game() {
+    //computer generates random word from words array
+    randomWord = words[Math.floor(Math.random() * words.length)];
 
+    // split the individual word into separate arrays, and store in new array 
+    lettersOfWord = randomWord.split("");
 
+    //store length of word in blanks, for later use
+    blanks = lettersOfWord.length;
 
+    //creating a loop to generate "_" for each letter in array stored in blanks
+    for (var i = 0; i < blanks; i++) {
+        blanksAndCorrect.push("_");
+    }
 
-alert("You Have 10 Guesses")
+    //showing the "_" within HTML
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
 
-const answer = 4;
-const guess = prompt("whats your guess?")
-
-for (i = 0; i < 10; i++) {
-    if (answer==guess) {
-
-        alert("you guess correctly!");
-        break;
-
-    } else { guess = prompt('please try again');
- }
-
-
-
+    //console logging 
+    console.log(randomWord);
+    console.log(lettersOfWord)
+    console.log(blanks)
+    console.log(blanksAndCorrect)
 }
 
 
 
+const pug= document.getElementById('pug');
+const shitzu = document.getElementById('shitzu');
+const bulldog = document.getElementById('bulldog');
 
 
+    //pug / Image
+    //---------------------------
+    if (randomWord === words[0]) {
+        
+
+        document.getElementById("image").src = "./assets/images/pug.jpg";
+    }
+    // shitzu /Image
+    //---------------------------
+    else if (randomWord === words[1]) {
+
+        document.getElementById("image").src = "./assets/images/Shitzu";
+    }
+    //bulldog / Image
+    //---------------------------
+    else if (randomWord === words[2]) {
+
+        document.getElementById("image").src = "./assets/images/bulldog.jpg";
+    }
+;
+
+    //__________________________________________________________
+    //RESET FUNCTION
+    //__________________________________________________________
+    function reset() {
+        guessesRemaining = 9;
+        wrongGuess = [];
+        blanksAndCorrect = [];
+        Game()
+    }
+
+    //__________________________________________________________
+    //CHECK LETTERS/COMPARE FUNCTION
+    //__________________________________________________________
+
+    //If/Else, to see if letter selected matches random word
+    function checkLetters(letter) {
+        let letterInWord = false;
+        //if the generated randomword is equal to the letter entered... then variable is true
+        for (var i = 0; i < blanks; i++) {
+            if (randomWord[i] == letter) {
+                letterInWord = true;
+            }
+        }
+        //if letterInWord (false)
+        if (letterInWord) {
+            //check each letter to see if it matches word
+            for (var i = 0; i < blanks; i++) {
+                if (randomWord[i] == letter) {
+                    blanksAndCorrect[i] = letter;
+                }
+            }
+        }
+        //otherwise, push the incorrect guess in the wrong guesses section, and reduce remaining guesses
+        else {
+            wrongGuess.push(letter);
+            guessesRemaining--;
+        }
+        console.log(blanksAndCorrect);
+    }
+
+    //__________________________________________________________
+    //FINAL COMPLETE FUNCTION
+    //__________________________________________________________
+
+    //check to see if player won...
+    function complete() {
+        console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
+
+        //if WON...then alert,  display image and reset new round
+        if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
+            wins++;
+            alert('You Win!');
+
+            reset()
+            //display wins on screen
+            document.getElementById("winstracker").innerHTML = " " + wins;
+
+            //if LOST...then alert and reset new round
+        } else if (guessesRemaining === 0) {
+            losses++;
+            alert ('Try Again');
+            reset()
+
+            document.getElementById("losstracker").innerHTML = " " + losses;
+        }
+        //display losses on screen && guesses remaining countdown
+        document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join(" ");
+        document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
+    }
 
 
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    //_____________________________________________________
+    // EXECUTE CODE 
+    //_____________________________________________________
 
+    //call start game function
+    Game()
 
+    //check for keyup, and convert to lowercase then store in guesses
+    document.onkeyup = function (event) {
+        let guesses = String.fromCharCode(event.keyCode).toLowerCase();
+        //check to see if guess entered matches value of random word
+        checkLetters(guesses);
+        //process wins/loss 
+        complete();
+        //store player guess in console for reference 
+        console.log(guesses);
 
-
-
-
+        //display/store incorrect letters on screen
+        document.getElementById("playerguesses").innerHTML = "  " + wrongGuess.join(" ");
+    } 
